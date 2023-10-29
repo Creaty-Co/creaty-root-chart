5ddc93f3bfd3b6f95b4d298ede6b202c170fb094
GentleEnvy <komarov.sergei163@gmail.com>
2023-10-29 19:19:41 +0300

    ✨ Create stage node pool resource
    - Add a new file `terraform/stage_node_pool.tf` to create a stage node pool resource for the project.
    - The node pool resource is created using the `digitalocean_kubernetes_node_pool` block.
    - The `count` parameter is set to 2, indicating that two node pools will be created.
    - The `cluster_id` parameter is set to the ID of the Kubernetes cluster.
    - The `name` parameter is set to "pool-2gb" as the name of the node pool.
    - The `size` parameter is set to "s-1vcpu-2gb" to specify the size of the nodes in the pool.
    - The `node_count` parameter is set to 1, indicating that each node pool will have one node.

---
+++ terraform/stage_node_pool.tf
@@ -0,0 +1,8 @@
+resource "digitalocean_kubernetes_node_pool" "stage_node_pool" {
+  count = 2
+
+  cluster_id = digitalocean_kubernetes_cluster.k8s_cluster.id
+  name       = "pool-2gb"
+  size       = "s-1vcpu-2gb"
+  node_count = 1
+}
